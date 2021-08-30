52836abe52f363eb5b9ed6d2e4ecb7e1eab1afdd
Antonio Massa <antonio.massa@zehus.it>
2021-08-30 16:21:18 +0200

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Massa Antonio on 30/08/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
